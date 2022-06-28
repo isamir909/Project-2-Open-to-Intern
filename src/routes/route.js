517b95027751bd1d22d02816c2598d112740206b
isamir909 <samirlohiya909@gmail.com>
2022-06-28 17:47:27 +0530

    added college controller

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,3 +1,33 @@
-const { Router } = require("express");
+const express = require("express")
+const router = express.Router();
+const collegeController = require("../controller/collegeController");
 
-module.exports=Router
+
+
+
+
+// Create a college - a document for each member of the group
+
+// The logo link will be provided to you by the mentors. This link is a s3 (Amazon's Simple Service) url. Try accessing the link to see if the link is public or not.
+
+// Endpoint: BASE_URL/functionup/colleges
+// ## College Model
+// { name: { mandatory, unique, example iith}, fullName: {mandatory, example `Indian Institute of Technology, Hyderabad`}, logoLink: {mandatory}, isDeleted: {boolean, default: false} }
+
+let validate=function(value){
+    if(value.trim()===""|| value===null) return false
+    if(typeof value==="string" ||typeof value==="undefined")return false
+    return true;
+
+}
+
+
+router.post('/functionup/colleges',async function(req,res){
+    let collegeData=req.body
+
+    let saveData=await  collegeDB.create(collegeData)
+
+})
+
+
+module.exports=router
