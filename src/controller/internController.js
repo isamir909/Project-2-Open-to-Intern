33d2552e0fadfaf4d0f22c6c16475160cb454de1
Sameer182002 <1978sameerbajaj@gmail.com>
2022-06-29 12:34:42 +0530

    Finally InternController Completed

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,45 +1,45 @@
-const internModel= require("../Models/InternModel")
-let collegeModel=require('../Models/CollegeModel')
-const {isValid,isValidName,isValidEmail,isValidMobile}=require("../Validator/validation")
+const internModel = require("../Models/InternModel")
+let collegeModel = require('../Models/CollegeModel')
+const { isValid, isValidName, isValidEmail, isValidMobile } = require("../Validator/validation")
 
-// { name: {mandatory}, email: {mandatory, valid email, unique}, mobile: {mandatory, valid mobile number, unique}, collegeId: {ObjectId, ref to college model, isDeleted: {boolean, default: false}}
-const createIntern = async function (req,res){
-    const data = req.body
-    const{name,email,mobile,collegeName}=req.body
 
-    if(Object.keys(data).length==0) return res.status(400).send({status:false, msg:"Body should not be empty "})
-    if(!("name" in data ) || !("email" in data) || !("mobile" in data) || !("collegeName" in data)) return res.status(400).send({status:false,msg:"Don't Skip The Required Attributes ('name','email','mobile','collegeName') "})
+const createIntern = async function (req, res) {
+    try {
+        const data = req.body
+        const { name, email, mobile, collegeName } = req.body
 
-    if(!isValid(name)) return res.status(400).send({status:false,msg:"Don't Left name attribute empty"})
-    if(!isValidName(name)) return res.status(400).send({status:false,msg:"Pls Enter Valid name"})
+        const lowerCollegeName = collegeName.toString().toLowerCase()
+        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, msg: "Body should not be empty " })
+        if (!("name" in data) || !("email" in data) || !("mobile" in data) || !("collegeName" in data)) return res.status(400).send({ status: false, msg: "Don't Skip The Required Attributes ('name','email','mobile','collegeName') " })
 
-    if(!isValid(email)) return res.status(400).send({status:false,msg:"Don't left email attribute empty"})
-    if(!isValidEmail(email)) return res.status(400).send({status:false,msg:"Pls Enter Valid Email"})
+        if (!isValid(name)) return res.status(400).send({ status: false, msg: "Don't Left name attribute empty" })
+        if (!isValidName(name)) return res.status(400).send({ status: false, msg: "Pls Enter Valid name" })
 
-    if(!isValid(mobile)) return res.status(400).send({status:false,msg:"Don't left mobile attribute empty"})
-    if(!isValidMobile(mobile)) return res.status(400).send({status:false,msg:"Pls Enter Valid PAN India Number"})
-    
-    if(!isValid(collegeName)) return res.status(400).send({status:false,msg:"Don't left college attribute empty and enter in shortname"})
-    if(!isValidName(collegeName)) return res.status(400).send({status:false,msg:"Pls Enter Valid College Name"})
-    
-    
-    let checkUniqueNo= await internModel.findOne({$and:[{mobile:req.body.mobile},{email:req.body.email }]}).select({mobile:1,_id:0,email:1})
-    console.log(checkUniqueNo.mobile)
-    console.log(checkUniqueNo.email)
-    if(checkUniqueNo) return res.status(400).status({status:false,msg:"Pls Use Unique Mobile No. .....This Mobile No. Already Exists "})
-    
-    // if("email" in data){
-    // let checkunique= await internModel.findOne({email:req.body.email}).select({email:1,_id:0})
-    // console.log(checkunique)
-    // if (checkunique) return res.status(400).send({status:false,msg:"Pls Use Unique Email....This Email Already Exists"})
-    // }
+        if (!isValid(email)) return res.status(400).send({ status: false, msg: "Don't left email attribute empty" })
+        if (!isValidEmail(email)) return res.status(400).send({ status: false, msg: "Pls Enter Valid Email" })
 
-    let cljId=await collegeModel.findOne({name:collegeName}).select({_id:1,isDeleted:1})
-    if(!cljId) return res.status(400).send({status:false,msg:"Pls Enter Valid CollegName"})
-    data.collegeId=cljId
+        if (!isValid(mobile)) return res.status(400).send({ status: false, msg: "Don't left mobile attribute empty" })
+        if (!isValidMobile(mobile)) return res.status(400).send({ status: false, msg: "Pls Enter Valid PAN India Number" })
 
-    let createIntern=await internModel.create(data)
-    res.send({status:true,msg:"Intern Created",data:createIntern})
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, msg: "Don't left college attribute empty and enter in shortname" })
+        if (!isValidName(collegeName)) return res.status(400).send({ status: false, msg: "Pls Enter Valid College Name" })
 
+        let checkunique = await internModel.findOne({ email: email })
+        if (checkunique) return res.status(400).send({ status: false, msg: "Pls Use Unique Email" })
+
+        let checkuniqueNo = await internModel.findOne({ mobile: mobile })
+        if (checkuniqueNo) return res.status(400).send({ status: false, msg: "Pls Enter Unique Mobile No." })
+
+        let cljId = await collegeModel.findOne({ name: lowerCollegeName }).select({ _id: 1 })
+        if (cljId.isDeleted == true) return res.status(400).send({ status: false, msg: "Sorry This College Is Deleted" })
+        if (!cljId) return res.status(400).send({ status: false, msg: "Pls Enter Valid CollegName" })
+        data.collegeId = cljId
+
+        let createIntern = await internModel.create(data)
+        res.send({ status: true, msg: "Intern Created", data: createIntern })
+    }
+    catch (err) {
+        res.status(500).send({ status: false, msg: err.message })
+    }
 }
-module.exports.createIntern=createIntern
+module.exports.createIntern = createIntern
